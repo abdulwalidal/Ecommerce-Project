0adb2dd05fa92353f30d3e63cb6ed922fa3f9553
Abdul Wali Dal <148618788+abdulwalidal@users.noreply.github.com>
2026-03-27 16:38:14 +0500

    feat: add Product model and controller for product management

---
+++ backend/src/main/java/com/ecommerce/project/model/Product.java
@@ -0,0 +1,31 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long productId;
+    private String productName;
+    private String productDescription;
+    private Integer quantity;
+    private double price;
+    private double specialPrice;
+    private double discount;
+    private String image;
+
+    @ManyToOne
+    @JoinColumn (name = "category_id")
+    private Category category;
+
+
+
+}
