d1817620d6aaaa25c175218d8de64ca4d26013dc
Abdul Wali Dal <148618788+abdulwalidal@users.noreply.github.com>
2026-03-27 16:38:38 +0500

    feat: implement ProductService and ProductResponse for product management

---
+++ backend/src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -0,0 +1,41 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
+import com.ecommerce.project.model.Category;
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.CategoryDTO;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.repository.CategoryRepository;
+import com.ecommerce.project.repository.ProductRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    @Override
+    public ProductDTO addProduct(Long categoryId, Product product) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(()-> new ResourceNotFoundException("category", "categoryId", categoryId));
+
+        product.setCategory(category);
+        product.setImage("default.png");
+
+        double specialPrice = product.getPrice() -
+                             ((product.getDiscount() * 0.01) * product.getPrice());
+        product.setSpecialPrice(specialPrice);
+        Product savedProduct = productRepository.save(product);
+        return modelMapper.map(savedProduct, ProductDTO.class);
+
+
+    }
+}
