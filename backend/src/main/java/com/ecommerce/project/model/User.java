570dbe8aecfe3c8dc29fa4cc0cce872ef9f1e982
Abdul Wali Dal <148618788+abdulwalidal@users.noreply.github.com>
2026-05-14 22:01:51 +0500

    feat: add User entity for user management

---
+++ backend/src/main/java/com/ecommerce/project/model/User.java
@@ -0,0 +1,47 @@
+package com.ecommerce.project.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table (name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size (max = 20)
+
+    private String userName;
+
+    @NotBlank
+    @Size (max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size (max = 120)
+    private String password;
+
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+                 fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+               joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+}
